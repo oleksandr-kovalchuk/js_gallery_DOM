6f724abb402e158ed5f255bb2b6386c94f40734b
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-10-02 11:06:37 +0300

    make build

---
+++ index.7a2c8f98.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#thumbs"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");t.src=r.href});
+//# sourceMappingURL=index.7a2c8f98.js.map
